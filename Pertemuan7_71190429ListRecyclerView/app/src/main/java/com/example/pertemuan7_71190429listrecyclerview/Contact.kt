59263d5aee0aa394810c42a215aa5f947024ba72
YohanesVito <yohanes.vito@ti.ukdw.ac.id>
2021-10-18 21:47:33 +0700

    tambah pertemuan7

---
+++ Pertemuan7_71190429ListRecyclerView/app/src/main/java/com/example/pertemuan7_71190429listrecyclerview/Contact.kt
@@ -0,0 +1,3 @@
+package com.example.pertemuan7_71190429listrecyclerview
+
+data class Contact (val contactName: String, val contactNumber: String, val contactImage: Int, val contactAddress: String, val contactEmail: String)
