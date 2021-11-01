28a56956df76571b1bb5be868c0cbdca3c8fdbb3
YohanesVito <yohanes.vito@ti.ukdw.ac.id>
2021-11-01 11:48:22 +0700

    tambah pertemuan 8

---
+++ Pertemuan8_71190429_ToolbarViewPager/app/src/main/java/com/example/pertemuan8_71190429_toolbarviewpager/FragmentSatu.kt
@@ -0,0 +1,16 @@
+package com.example.pertemuan8_71190429_toolbarviewpager
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class FragmentSatu: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View = inflater.inflate(R.layout.fragment_satu, container, false)
+}
+
---
+++ Pertemuan8_71190429_ToolbarViewPager/app/src/main/java/com/example/pertemuan8_71190429_toolbarviewpager/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.pertemuan8_71190429_toolbarviewpager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.widget.ViewPager2
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        //memasang Toolbar
+        setSupportActionBar(findViewById(R.id.toolbar_default))
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+
+        //instansiasi ViewPager
+        val viewPager = findViewById<ViewPager2>(R.id.pager)
+
+        //Memasukkan seluruh fragment ke dalam ArrayList
+        val listFragment: ArrayList<Fragment> = arrayListOf(FragmentSatu(), FragmentDua())
+
+        //instansiasi Adapter untuk ViewPager
+        val pagerAdapter = PagerAdapter(this, listFragment)
+        viewPager.adapter = pagerAdapter
+    }
+    class PagerAdapter(val activity: AppCompatActivity, val listFragment: ArrayList<Fragment>){
+        override fun getItemCount(): Int = listFragment.size
+
+        override fun createFragment(position: Int): Fragment = listFragment.get(position)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        getMenuInflater().inflate(R.menu.menu_default, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.menu_profile -> {
+            Toast.makeText(this,"Menu Profile",Toast.LENGTH_LONG).show()
+            true
+        }
+        R.id.menu_settings -> {
+            Toast.makeText(this,"Menu Settings",Toast.LENGTH_LONG).show()
+            true
+        }
+        else -> {
+            super.onOptionsItemSelected(item)
+        }
+    }
+}
