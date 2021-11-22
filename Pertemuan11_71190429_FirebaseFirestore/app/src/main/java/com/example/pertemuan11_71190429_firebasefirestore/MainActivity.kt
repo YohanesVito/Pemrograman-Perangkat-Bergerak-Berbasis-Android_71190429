d7bee1429ed6c4eaf8f813bb8910f62d7b84cc85
YohanesVito <yohanes.vito@ti.ukdw.ac.id>
2021-11-22 23:24:56 +0700

    tambah pertemuan 11

--- Pertemuan11_71190429_FirebaseFirestore/app/src/main/java/com/example/pertemuan11_71190429_firebasefirestore/MainActivity.kt
+++ Pertemuan11_71190429_FirebaseFirestore/app/src/main/java/com/example/pertemuan11_71190429_firebasefirestore/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.pertemuan11_71190429_firebasefirestore
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import com.google.firebase.firestore.FirebaseFirestore
 
 class MainActivity : AppCompatActivity() {
@@ -10,6 +13,19 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val nama = findViewById<EditText>(R.id.etNama)
+        val nim = findViewById<EditText>(R.id.etNIM)
+        val ipk = findViewById<EditText>(R.id.etIPK)
+
+        val btnsimpan = findViewById<Button>(R.id.btnSimpan)
+        val btncari = findViewById<Button>(R.id.btnCari)
+
+        val hasilNama = findViewById<TextView>(R.id.tvHasilNama)
+        val hasilNIM = findViewById<TextView>(R.id.tvHasilNIM)
+        val hasilIPK = findViewById<TextView>(R.id.tvHasilIPK)
+
+
         firestore = FirebaseFirestore.getInstance()
 
     }
