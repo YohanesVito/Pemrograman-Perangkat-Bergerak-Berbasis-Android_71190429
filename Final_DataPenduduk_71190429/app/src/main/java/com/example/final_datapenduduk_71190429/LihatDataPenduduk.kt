222241c7699c7eb6a606d5dc204006b7fe007f38
YohanesVito <yohanes.vito@ti.ukdw.ac.id>
2021-12-17 15:17:09 +0700

    tambah intent

---
+++ Final_DataPenduduk_71190429/app/src/main/java/com/example/final_datapenduduk_71190429/LihatDataPenduduk.kt
@@ -0,0 +1,13 @@
+package com.example.final_datapenduduk_71190429
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class LihatDataPenduduk: AppCompatActivity(){
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lihatdatapenduduk)
+
+    }
+}
---
+++ Final_DataPenduduk_71190429/app/src/main/java/com/example/final_datapenduduk_71190429/TambahDataPenduduk.kt
@@ -0,0 +1,13 @@
+package com.example.final_datapenduduk_71190429
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class TambahDataPenduduk: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tambahdatapenduduk)
+
+    }
+}
