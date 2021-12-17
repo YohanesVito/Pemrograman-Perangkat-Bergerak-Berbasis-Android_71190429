5a9b9f96cc46c2858b319ab4a37e1bb0db29a75d
YohanesVito <yohanes.vito@ti.ukdw.ac.id>
2021-12-17 15:52:32 +0700

    tambah intent dan logout

--- Final_DataPenduduk_71190429/app/src/main/java/com/example/final_datapenduduk_71190429/SecondActivity.kt
+++ Final_DataPenduduk_71190429/app/src/main/java/com/example/final_datapenduduk_71190429/SecondActivity.kt
@@ -1,7 +1,10 @@
 package com.example.final_datapenduduk_71190429
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class SecondActivity : AppCompatActivity(){
@@ -15,6 +18,24 @@ class SecondActivity : AppCompatActivity(){
         val message = findViewById<TextView>(R.id.welcomeText)
         message.text = "Welcome ${username}, your password is ${password}"
 
+        val btnlihatdata = findViewById<Button>(R.id.bt_lihatdata)
+        val btntambahdata = findViewById<Button>(R.id.bt_tambahdata)
+        val btnlogout = findViewById<Button>(R.id.bt_logout)
+        btnlihatdata.setOnClickListener {
+            val i = Intent(this, LihatDataPenduduk::class.java);
+            startActivity(i)
+        }
+
+        btntambahdata.setOnClickListener {
+            val i = Intent(this, TambahDataPenduduk::class.java);
+            startActivity(i)
+        }
+
+        btnlogout.setOnClickListener {
+            Toast.makeText(this,"Log Out Berhasil!", Toast.LENGTH_SHORT).show()
+            val i = Intent(this, MainActivity::class.java);
+            startActivity(i)
+        }
     }
 
 }
